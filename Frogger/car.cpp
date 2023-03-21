f922df5bfcfb2ab7cdb8087ff3928cda1d519744
737716 <ntam101@gmail.com>
2023-03-21 09:58:42 -0600

    guh

--- Frogger/car.cpp
+++ Frogger/car.cpp
@@ -1,1 +1,6 @@
+#include <SFML/Graphics.hpp>
 #include "car.h"
+
+void car:: draw(sf::RenderWindow& window) {
+	sf::RectangleShape le_car(sf::Vector2f(100, 50));
+}
--- Frogger/car.h
+++ Frogger/car.h
@@ -1,11 +1,21 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "globals.h"
 using namespace std;
 
 class car {
 private:
 	float xpos;
 	float ypos;
+	int green = rand() % 100 + 200;
+	int blue = rand() % 100 + 200;
+	int direction = RIGHT;
+public:
+	car(int x, int y, int direction);
+	void draw(sf::RenderWindow& window);
+	void move();
+	bool collide(int x, int y);
+	void printInfo() { cout << "I am a car and my position is" << xpos << ", " << ypos << endl; }
 };
 
