e1971530c4e05ffc81d8015d2de481dfe740f2df
737716 <ntam101@gmail.com>
2023-03-22 09:49:42 -0600

    guh

--- Frogger/car.cpp
+++ Frogger/car.cpp
@@ -2,5 +2,29 @@
 #include "car.h"
 
 void car:: draw(sf::RenderWindow& window) {
-	sf::RectangleShape le_car(sf::Vector2f(100, 50));
+	sf::RectangleShape vehicle(sf::Vector2f(100, 50));
+	vehicle.setFillColor(sf::Color(250, green, blue));
+	vehicle.setPosition(xpos, ypos);
+	window.draw(vehicle);
+}
+
+void car::move() {
+	if (direction == LEFT) {
+		if (xpos < -100)
+			xpos = 100;
+		xpos -= .1;
+	}
+	else if (direction == RIGHT) {
+		if (xpos > 1100)
+			xpos = -100;
+		xpos += .1;
+	}
+}
+bool car::collide(int x, int y) {
+	if (xpos + 100 < x && xpos >= x && ypos + 50 > y && ypos <= y) {
+		cout << "U DED" << endl;
+		return true;
+	}
+	else
+		return false;
 }
